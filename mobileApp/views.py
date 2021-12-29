f4bf3f9aa2f1c1fe679d3c48f7ed9dcf37fc4b90
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-29 23:17:54 +0600

    message show

--- mobileApp/views.py
+++ mobileApp/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.http import JsonResponse
+from django.contrib import messages
 # Create your views here.
 from mobileApp.models import Mobile
 
@@ -18,7 +19,11 @@ def add_mobile(request):
 
     jan_code_exist = Mobile.objects.filter(jan_code=jan_code).exists()
     if jan_code_exist:
-        return render(request, 'home.html', )
+        messages.error(
+            request,
+            "This code is already exist. Please provide unique JAN Code",
+        )
+        return render(request, 'home.html')
     else:
         mobile_ins = Mobile(brand_name=brand_name, model=model, jan_code=jan_code, color=color)
         mobile_ins.save()
@@ -26,11 +31,8 @@ def add_mobile(request):
 
 
 def mobile_list(request):
-
     d = request.POST.get("search_text")
     print(d)
-
-
 
     mobile_list = Mobile.objects.all()
     context = {
@@ -54,9 +56,9 @@ def delete_mobile(request, id):
 
 
 def delete_mobiles(request, *args, **kwargs):
-    if request.method=="POST":
+    if request.method == "POST":
         print("delete multiple mobiles...")
-        mobile_ids=request.POST.getlist('id[]')
+        mobile_ids = request.POST.getlist('id[]')
         for id in mobile_ids:
             mobile = Mobile.objects.get(pk=id)
             mobile.delete()
@@ -69,10 +71,13 @@ def search_mobile_list(request):
     print("search hitting: ")
     search_text = request.POST.get("series")
     print(search_text)
-    mobile = Mobile.objects.filter(Q(model__contains=search_text) | Q(jan_code__contains=search_text))
-    print("mobile: ", mobile)
+    mobile = Mobile.objects.filter(Q(model__contains=search_text) | Q(jan_code__contains=search_text)).values()
+    print("mobile: ", list(mobile))
+
     context = {
         "mobile_list": mobile
     }
 
-    return render(request, 'mobile-list.html', context)
+
+    return JsonResponse(data=list(mobile), safe=False)
+
